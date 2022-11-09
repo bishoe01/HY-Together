866aa272d511adf4dba05e60c4a643d924f92999
bishoe01 <bishoe01@gmail.com>
2022-11-09 14:46:05 +0900

    origin:

--- hytogether/src/App.jsx
+++ hytogether/src/App.jsx
@@ -6,27 +6,29 @@ import Navigation from './components/layout/navigation';
 import Department from './components/department';
 import { useEffect, useState } from 'react';
 import Home from './components/layout/home';
-
+import { Fade } from 'react-reveal';
 function App() {
   const [selfInfo, setselfInfo] = useState(null);
   useEffect(() => {
     fetch("studentData.json")
-    .then(res => res.json())
-    .then(data => setselfInfo(data.department))
+      .then(res => res.json())
+      .then(data => setselfInfo(data.department))
     return () => {
       console.log(selfInfo);
     };
   }, []);
   return (
     <>
-    <Navigation/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      {/* <Route path="/about" element={<About/>}/> */}
-      <Route path={`/${selfInfo}`} element={<Department deparment={selfInfo}/>}/>
-      {/* <Route path="/contact" element={<Contact/>}/> */}
-    </Routes>
-    <Footer/>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<body>
+          <Home />
+        </body>} />
+        {/* <Route path="/about" element={<About/>}/> */}
+        <Route path={`/${selfInfo}`} element={<Department deparment={selfInfo} />} />
+        {/* <Route path="/contact" element={<Contact/>}/> */}
+      </Routes>
+      <Footer />
     </>
   );
 }
